709395fbf33283d682858de3e982692ac006d9aa
adamrichardson14 <45330519+adamrichardson14@users.noreply.github.com>
2020-11-15 01:08:45 +0000

    manifest typo

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,11 @@ module.exports = {
     ...(process.env.NODE_ENV === 'production'
       ? {
           '@fullhuman/postcss-purgecss': {
-            content: ['./components/**/*.jsx', './pages/**/*.jsx'],
+            content: [
+              './components/**/*.jsx',
+              './pages/**/*.jsx',
+              './styles/styles.css',
+            ],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
           },
