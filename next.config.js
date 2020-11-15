080b7efcdac1e3c7a8605015114a702ec0f91e3e
adamrichardson14 <45330519+adamrichardson14@users.noreply.github.com>
2020-11-15 16:04:09 +0000

    added extra image domains

--- next.config.js
+++ next.config.js
@@ -6,7 +6,18 @@ module.exports = withPWA({
     dest: 'public',
   },
   images: {
-    domains: ['res-3.cloudinary.com', 'res-2.cloudinary.com'],
+    domains: [
+      'res-1.cloudinary.com',
+      'res-2.cloudinary.com',
+      'res-3.cloudinary.com',
+      'res-4.cloudinary.com',
+      'res-5.cloudinary.com',
+      'res-6.cloudinary.com',
+      'res-7.cloudinary.com',
+      'res-8.cloudinary.com',
+      'res-9.cloudinary.com',
+      'res-10.cloudinary.com',
+    ],
     deviceSizes: [320, 420, 768, 1024, 1200],
   },
 });
